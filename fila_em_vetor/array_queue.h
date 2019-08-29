e6d47b7e37d7bfb60abbee00d93b2a7bb1a9d44e
Andre Merlo <casagrandamerlo@gmail.com>
2019-08-29 16:21:39 -0300

    resolvida atividade de fila em vetor

--- fila_em_vetor/array_queue.h
+++ fila_em_vetor/array_queue.h
@@ -1,4 +1,4 @@
-// Copyright [2018] <COLOQUE SEU NOME AQUI...>
+// Copyright [2018] André Casagranda Merlo - 17200434
 #ifndef STRUCTURES_ARRAY_QUEUE_H
 #define STRUCTURES_ARRAY_QUEUE_H
 
@@ -46,3 +46,86 @@ class ArrayQueue {
 }  // namespace structures
 
 #endif
+
+
+template<typename T>
+structures::ArrayQueue<T>::ArrayQueue() {
+    max_size_ = DEFAULT_SIZE;
+    contents = new T[max_size_];
+    size_ = 0;
+    begin_ = 0;
+    end_ = -1;
+}
+
+template<typename T>
+structures::ArrayQueue<T>::ArrayQueue(std::size_t max) {
+    max_size_ = max;
+    contents = new T[max];
+    size_ = 0;
+    begin_ = 0;
+    end_ = -1;
+}
+
+template<typename T>
+structures::ArrayQueue<T>::~ArrayQueue() {
+    delete [] contents;
+}
+
+template<typename T>
+void structures::ArrayQueue<T>::enqueue(const T& data) {
+    if (full()) {
+        throw std::out_of_range("fila cheia");
+    } else {
+        end_ = (end_ + 1) % max_size_;
+        contents[end_] = data;
+        size_++;
+    }
+}
+
+template<typename T>
+T structures::ArrayQueue<T>::dequeue() {
+    if (empty()) {
+        throw std::out_of_range("pilha vazia");
+    } else {
+        T aux = contents[begin_];
+        begin_ = (begin_ + 1) % max_size_;
+        size_--;
+        return aux;
+    }
+}
+
+template<typename T>
+T& structures::ArrayQueue<T>::back() {
+    if (empty()) {
+        throw std::out_of_range("pilha vazia");
+    } else {
+        return contents[end_];
+    }
+}
+
+template<typename T>
+void structures::ArrayQueue<T>::clear() {
+    size_ = 0;
+    begin_ = 0;
+    end_ = -1;
+}
+
+template<typename T>
+std::size_t structures::ArrayQueue<T>::size() {
+    return size_;
+}
+
+template<typename T>
+std::size_t structures::ArrayQueue<T>::max_size() {
+    return max_size_;
+}
+
+template<typename T>
+bool structures::ArrayQueue<T>::empty() {
+    return size_ == 0;
+}
+
+template<typename T>
+bool structures::ArrayQueue<T>::full() {
+    return size_ == max_size_;
+}
